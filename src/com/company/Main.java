450abdbbfa01bc5fd48438622073c404ed995ff8
Eiddom Micael <eiddom.micael@student.kyh.se>
2020-09-10 12:22:09 +0200

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //want to show all digits ex: 2.000
+
+        System.out.println("Mean of 1, 1, 3, 3: " + meanCalc( 1, 1, 3,  3));
+        System.out.println("Mean of 1, 2, 3, 4: " + meanCalc( 1, 2, 3, 4));
+        System.out.println("Mean of 1, -2, 3, -4: " + meanCalc( 1,  -2, 3, -4));
+        System.out.println("Mean of -1, -11, 24, 2: " + meanCalc( -1, -11, 24, 2));
+    }
+
+    public static String meanCalc(int a, int b, int c, int d) {
+        // gör en sträng av "mean"
+
+        double mean = (a + b + c + d) / 4.0;
+        String meanString = "" + mean;
+
+        return meanString;
+    }
+}
