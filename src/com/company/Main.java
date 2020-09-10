a8cb60007fcc6622b43f74577fadc90380dc42f6
Eiddom Micael <eiddom.micael@student.kyh.se>
2020-09-10 14:51:32 +0200

    .println -11, -113

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,6 +9,7 @@ public class Main {
         System.out.println("Mean of 1, 2, 3, 4: " + meanCalc( 1, 2, 3, 4));
         System.out.println("Mean of 1, -2, 3, -4: " + meanCalc( 1,  -2, 3, -4));
         System.out.println("Mean of -1, -11, 24, 2: " + meanCalc( -1, -11, 24, 2));
+        System.out.println("Mean of -11, -113, 124, 23: " + meanCalc( -11, -111, 124, 23));
     }
 
     public static String meanCalc(int a, int b, int c, int d) {
